c71fccd34d7c71c8ed94a5a97274204ca918b8c7
sienna0715 <5609373@naver.com>
2025-02-24 16:37:46 +0900

    💚 Fix build(js file name)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
     build: {
         outDir: "dist",  // 빌드된 JS 파일이 저장될 폴더ㅌ
         sourcemap: true, // 디버깅을 위한 소스맵 추가 (선택)
         emptyOutDir: true, // 빌드 전에 기존 파일 삭제
+        rollupOptions: {
+            output: {
+              entryFileNames: 'index.js', // JS 파일을 dist에 저장
+            },
+        },
     },
 });
